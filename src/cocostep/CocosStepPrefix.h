a73b3c8f0ca1ade3d0952c8b5c3c2818abdfd882
AN Indie <an.indian.indie@gmail.com>
2011-01-31 21:30:01 +0530

    Restructured files.

--- src/cocostep/CocosStepPrefix.h
+++ src/cocostep/CocosStepPrefix.h
@@ -1,6 +1,6 @@
 #import <Cocoa/Cocoa.h>
 #import <CGGeometry.h>
 #import <UIImage.h>
-#import <Utils.h>
 #import <UITouch.h>
 #import <UIApplication.h>
+#import <Objc2BackPort.h>
---
+++ src/cocostep/UIKit/UIKit.h
@@ -0,0 +1,6 @@
+#include <Cocoa/Cocoa.h>
+#include "CGGeometry.h"
+#include "Objc2BackPort.h"
+#include "UIApplication.h"
+#include "UIImage.h"
+#include "UITouch.h"
